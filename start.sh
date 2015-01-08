f2a80072230e96502b0cfab7102def946e6d3f85
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-01-08 02:35:32 -0500

    adding log folder check

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Variables
+LOG_DIR='log' #log folder, located in same level as this script
+
+# Check if log directory exists, if not, then created
+if [ ! -d "$LOG_DIR" ]; then
+  # Control will enter here if $LOG_DIR exists.
+  echo "NOTE: $LOG_DIR folder not detected, creating..."
+  mkdir -pv "$LOG_DIR"
+fi
 
 # Invoke the Forever module (to START our Node.js server).
 # NOTE: if log folder doesn't exist, this script will break, TODO: add folder check and if not exist, create
